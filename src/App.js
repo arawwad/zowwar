3df190db0f61141550d5e07d5b334d276d8155aa
abdallah awwad <a.awwad@procabnow.com>
2019-08-27 14:55:05 +0200

    layout scaffolding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Login = lazy(() => import('./routes/Login'));
+const Profile = lazy(() => import('./routes/Profile'));
+const Register = lazy(() => import('./routes/Register'));
+const Result = lazy(() => import('./routes/Result'));
 
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Profile} />
+        <Route path="/result" component={Result} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 export default App;
