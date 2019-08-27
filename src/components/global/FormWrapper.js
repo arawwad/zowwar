ba12d4f94d6560e3836b3d7a1a8f35a5302bb755
abdallah awwad <a.awwad@procabnow.com>
2019-08-27 15:35:38 +0200

    add css normalize

--- src/components/global/FormWrapper.js
+++ src/components/global/FormWrapper.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FormWrapper = ({children}) => (
-    <div>{children}</div>
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background: #0250c5;
+    background: linear-gradient(bottom, #0250c5, #d43f8d);
+    position: relative;
+    z-index: 1;
+`
+
+
+const FormWrapper = ({ children, title }) => (
+    <Container>{children}</Container>
 )
 
 export default FormWrapper;
